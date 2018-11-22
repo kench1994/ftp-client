210806f7dfa51669923dcb1b066c92db74853b7e
deniskovalchuk <deniskk25@gmail.com>
2018-11-23 00:39:55 +0300

    Handle input errors and EOF (Ctrl + D).

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,41 @@
  * This code is licensed under a MIT-style license.
  */
 
+#include <iostream>
 #include "user_interface.hpp"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::istream;
+using std::exception;
 
 int main(int argc, char * argv[])
 {
-    ftp::user_interface ui;
-    ui.run();
+    cin.exceptions(istream::failbit | istream::badbit);
+    
+    try
+    {
+        ftp::user_interface ui;
+        ui.run();
+    }
+    catch (const istream::failure & ex)
+    {
+        if (cin.eof())
+        {
+            return EXIT_SUCCESS;
+        }
+        else
+        {
+            cout << ex.what() << endl;
+            return EXIT_FAILURE;
+        }
+    }
+    catch (const exception & ex)
+    {
+        cout << ex.what() << endl;
+        return EXIT_FAILURE;
+    }
 
     return 0;
 }
