8eb5551ead7176aa10744010d6a07029cac6bb3e
Denis Kovalchuk <denis.kovalchuk.main@gmail.com>
2020-03-12 10:20:33 +0300

    Use consistent format of error messages.

--- src/ftp/client.cpp
+++ src/ftp/client.cpp
@@ -159,7 +159,7 @@ void client::stor(const string & local_file, const string & remote_file)
 
     if (!file)
     {
-        string error_msg = utils::format("Cannot open file: '%1%'.", local_file);
+        string error_msg = utils::format("Cannot open file %1%.", local_file);
         notify_of_error(error_msg);
         return;
     }
@@ -205,7 +205,7 @@ void client::retr(const string & remote_file, const string & local_file)
 
     if (!file)
     {
-        string error_msg = utils::format("Cannot create file: '%1%'.", local_file);
+        string error_msg = utils::format("Cannot create file %1%.", local_file);
         notify_of_error(error_msg);
         return;
     }
