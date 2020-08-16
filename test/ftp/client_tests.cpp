cb59b07370955802997f5d9e8c0cd306c1f85140
Denis Kovalchuk <denis.kovalchuk.main@gmail.com>
2020-08-16 11:21:34 +0300

    Add test for noop command.

--- test/ftp/client_tests.cpp
+++ test/ftp/client_tests.cpp
@@ -124,3 +124,29 @@ TEST_F(FtpClientTest, LoginTest)
                                           "221 Goodbye.\r\n");
     EXPECT_EQ(ftp_observer.get_errors(), "");
 }
+
+TEST_F(FtpClientTest, NoopCommandTest)
+{
+    test_ftp_observer ftp_observer;
+    ftp::client client;
+    client.subscribe(&ftp_observer);
+
+    ftp::command_result result = client.open("localhost", 2121);
+    EXPECT_EQ(result, ftp::command_result::ok);
+
+    result = client.login("user", "password");
+    EXPECT_EQ(result, ftp::command_result::ok);
+
+    result = client.noop();
+    EXPECT_EQ(result, ftp::command_result::ok);
+
+    result = client.close();
+    EXPECT_EQ(result, ftp::command_result::ok);
+
+    EXPECT_EQ(ftp_observer.get_replies(), "220 FTP server is ready.\r\n"
+                                          "331 Username ok, send password.\r\n"
+                                          "230 Login successful.\r\n"
+                                          "200 I successfully done nothin'.\r\n"
+                                          "221 Goodbye.\r\n");
+    EXPECT_EQ(ftp_observer.get_errors(), "");
+}
