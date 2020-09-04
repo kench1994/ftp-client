328deec2ef1fdd87bda4d46d9b20e4dc32d0195a
Denis Kovalchuk <denis.kovalchuk.main@gmail.com>
2020-09-04 17:27:45 +0300

    Add test for downloading file.

--- test/ftp/client_tests.cpp
+++ test/ftp/client_tests.cpp
@@ -39,6 +39,7 @@ class FtpClientTest : public ::testing::Test
 protected:
     static void SetUpTestSuite()
     {
+        std::filesystem::create_directory(m_downloadsDir);
         std::filesystem::create_directory(m_ftpServerDir);
         boost::filesystem::path pythonPath = boost::process::search_path("python3");
 
@@ -54,6 +55,7 @@ protected:
     {
         m_ftpServerProcess.terminate();
         std::filesystem::remove_all(m_ftpServerDir);
+        std::filesystem::remove_all(m_downloadsDir);
     }
 
     void TearDown() override
@@ -63,13 +65,21 @@ protected:
         {
             std::filesystem::remove_all(path);
         }
+
+        /* Clear downloads directory after each test. */
+        for (const auto & path : std::filesystem::directory_iterator(m_downloadsDir))
+        {
+            std::filesystem::remove_all(path);
+        }
     }
 
 private:
+    static const string m_downloadsDir;
     static const string m_ftpServerDir;
     static boost::process::child m_ftpServerProcess;
 };
 
+const string FtpClientTest::m_downloadsDir = "downloads";
 const string FtpClientTest::m_ftpServerDir = "test_server";
 boost::process::child FtpClientTest::m_ftpServerProcess;
 
@@ -517,6 +527,38 @@ TEST_F(FtpClientTest, UploadNonexistentFileTest)
     ASSERT_EQ(replies, "220 FTP server is ready.\r\n"
                        "331 Username ok, send password.\r\n"
                        "230 Login successful.\r\n"
+                       "229 Entering extended passive mode (|||1234|).\r\n"
+                       "125 Data connection already open. Transfer starting.\r\n"
+                       "226 Transfer complete.\r\n"
+                       "229 Entering extended passive mode (|||1234|).\r\n"
+                       "125 Data connection already open. Transfer starting.\r\n"
+                       "226 Transfer complete.\r\n"
+                       "221 Goodbye.\r\n");
+}
+
+TEST_F(FtpClientTest, DownloadTest)
+{
+    test_ftp_observer ftp_observer;
+    ftp::client client(&ftp_observer);
+
+    EXPECT_TRUE(client.open("localhost", 2121));
+    EXPECT_TRUE(client.login("user", "password"));
+    EXPECT_TRUE(client.binary());
+    EXPECT_TRUE(client.upload("../ftp/test_data/war_and_peace.txt", "war_and_peace.txt"));
+    EXPECT_TRUE(client.download("war_and_peace.txt", "downloads/war_and_peace.txt"));
+    EXPECT_TRUE(client.close());
+
+    /* Replace unpredictable data. */
+    string replies = ftp_observer.get_replies();
+
+    replies = regex_replace(replies,
+                            regex(R"(229 Entering extended passive mode \(\|\|\|\d{1,5}\|\)\.)"),
+                            "229 Entering extended passive mode (|||1234|).");
+
+    ASSERT_EQ(replies, "220 FTP server is ready.\r\n"
+                       "331 Username ok, send password.\r\n"
+                       "230 Login successful.\r\n"
+                       "200 Type set to: Binary.\r\n"
                        "229 Entering extended passive mode (|||1234|).\r\n"
                        "125 Data connection already open. Transfer starting.\r\n"
                        "226 Transfer complete.\r\n"
